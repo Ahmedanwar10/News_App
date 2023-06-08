fba78607e348a1bf7125d0bbb57718aeeb4ea996
Ali070 <Ali.zidan39@yahoo.com>
2023-06-08 20:51:29 +0300

    Initial Commit

---
+++ lib/layout/home/home_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  static String routeName = "HomeLayout";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.green,),
+    );
+  }
+}
